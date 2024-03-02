ffd992c8a5a094f68c48b68e040bb1e45a0f3e56
thatimoreira <thatimoreira1@gmail.com>
2024-03-02 19:23:48 -0300

    Allocated memory for node p and attributed its values

--- node_creation.c
+++ node_creation.c
@@ -6,11 +6,12 @@
 /*   By: tsoares- <tsoares-@student.42.rio>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/02 17:37:37 by tsoares-          #+#    #+#             */
-/*   Updated: 2024/03/02 19:02:43 by tsoares-         ###   ########.fr       */
+/*   Updated: 2024/03/02 19:19:12 by tsoares-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdlib.h>
+#include <stdio.h>
 
 struct node  // Declares the struct node
 {
@@ -20,9 +21,12 @@ struct node  // Declares the struct node
 
 int	main(void)
 {
-	struct node	*prev;
+	struct node	*prev; // node creation
 	struct node	*head;
 	struct node	*p;
 
+	p = (struct node *)malloc(sizeof(struct node)); // allocates memory for node p
+	scanf("%d", p->data); // the user inputs is being attributed to the data of p
+	p->next = NULL;
 	return (0);
 }
