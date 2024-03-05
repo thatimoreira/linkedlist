038d1ab3550843f1ad1d96baab693ea0a11ef4bb
thatimoreira <thatimoreira1@gmail.com>
2024-03-05 03:42:01 -0300

    Freed memory

--- node_creation.c
+++ node_creation.c
@@ -6,7 +6,7 @@
 /*   By: tsoares- <tsoares-@student.42.rio>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/02 17:37:37 by tsoares-          #+#    #+#             */
-/*   Updated: 2024/03/02 20:00:41 by tsoares-         ###   ########.fr       */
+/*   Updated: 2024/03/05 03:38:04 by tsoares-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,19 +27,35 @@ int	main(void)
 	int		n;
 	int		i;
 
-	printf("Number of elements");
+	printf("Number of elements: ");
 	scanf("%d", &n);
 	head = NULL;
 	for(i = 0; i < n; i++)
 	{
+		printf("Node %d: ", i);
 		p = (struct node *)malloc(sizeof(struct node)); // allocates memory for node p
-		scanf("%d", p->data); // the user input is being attributed to the data of p
+		scanf("%d", &p->data); // the user input is being attributed to the data of p
 		p->next = NULL; // attributes NULL as the value of node p
 		if (head == NULL) // if the linked list wasn't created
 			head = p; // makes the 1st node of the linked list and gives the value p to it
 		else
 			prev->next=p; // connects the previous node to node p (current node)
 		prev = p; // makes the last node
+	}
+	printf("Linked List Elements:\n");
+	p = head;
+	while (p != NULL)
+	{
+		printf("%d ", p->data);
+		p = p->next;
+	}
+	printf("\n");
+	p = head;
+	while (p != NULL)
+	{
+		struct node *temp = p;
+		p = p->next;
+		free(temp);
 	}
 	return (0);
 }
