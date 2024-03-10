b0741848b012190c90fcfd7376bb1f4a0c108715
thatimoreira <thatimoreira1@gmail.com>
2024-03-09 23:19:21 -0300

    Initialized, assigned values and connected the nodes

--- 00_singly-linked-list/ex00_node-creation01.c
+++ 00_singly-linked-list/ex00_node-creation01.c
@@ -4,6 +4,19 @@
 // Node declaration
 typedef struct  node
 {
-    int         data;
+    int         x;
     struct node *next;
 } node;
+
+int main(void)
+{
+    // Initializing nodes
+    node    root, elem2;
+
+    // Assigning values and connecting nodes
+    root.x = 15;
+    root.next = &elem2;
+    elem2.x = -2;
+    elem2.next = NULL;
+    return (0);
+}
