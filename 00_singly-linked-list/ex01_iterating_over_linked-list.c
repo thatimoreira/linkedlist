adacdc1a663fa91ad4e8f679a5639b918254ceff
thatimoreira <thatimoreira1@gmail.com>
2024-03-10 03:32:47 -0300

    Created a third node and freed it's memory

--- 00_singly-linked-list/ex01_iterating_over_linked-list.c
+++ 00_singly-linked-list/ex01_iterating_over_linked-list.c
@@ -1,6 +1,10 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+// Steps to iterate:                ARRAY X LINKED LIST
+// Array:                  i = 0          |        i < n        |         i++
+// Linked list:   node *current = &root   |   current != NULL   |  current→current.next
+
 typedef struct  node
 {
     int x;
@@ -16,7 +20,11 @@ int main(void)
     if (root.next == NULL)
         return (-1);
     root.next->x = -2;
-    root.next->next = NULL;
+    root.next->next = (node *)malloc(sizeof(node));
+    if (root.next->next == NULL)
+        return (-1);
+    (*root.next).next->x = 22;
+    (*root.next).next->next = NULL;
 
     node    *current;
 
@@ -28,6 +36,7 @@ int main(void)
         current = current->next;
     }
     printf("\n");
+    free(root.next->next);
     free(current);
     return (0);
 }
