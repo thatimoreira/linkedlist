064bebc519464fa3daaf80755191a539fda2ece8
thatimoreira <thatimoreira1@gmail.com>
2024-03-10 02:30:01 -0300

    Another way to access element 2

--- 00_singly-linked-list/ex00_node-creation01.c
+++ 00_singly-linked-list/ex00_node-creation01.c
@@ -18,5 +18,7 @@ int main(void)
     root.next = &elem2;
     elem2.x = -2;
     elem2.next = NULL;
+    printf("First element: %d\n", root.x);
+    printf("Second element: %d\n", root.next->x);
     return (0);
 }
