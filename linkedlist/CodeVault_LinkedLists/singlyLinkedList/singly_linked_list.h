f2eb1fe12705511fb532c15ac63c709be6bef322
thatimoreira <thatimoreira1@gmail.com>
2024-04-21 15:49:01 -0300

    Add ALLOCATION_FAILURE macro to handle memory allocation failures.

--- linkedlist/CodeVault_LinkedLists/singlyLinkedList/singly_linked_list.h
+++ linkedlist/CodeVault_LinkedLists/singlyLinkedList/singly_linked_list.h
@@ -6,7 +6,7 @@
 /*   By: tsoares- <tsoares-@student.42.rio>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/21 13:19:58 by tsoares-          #+#    #+#             */
-/*   Updated: 2024/04/21 13:21:25 by tsoares-         ###   ########.fr       */
+/*   Updated: 2024/04/21 13:48:02 by tsoares-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,6 +15,8 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+
+#define ALLOCATION_FAILURE -1
 
 typedef struct	s_node
 {
