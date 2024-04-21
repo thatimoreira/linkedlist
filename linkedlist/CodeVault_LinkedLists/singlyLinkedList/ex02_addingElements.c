2149c7253165b089edb58f292b92ecd6e1bd4d90
thatimoreira <thatimoreira1@gmail.com>
2024-04-21 13:36:02 -0300

    Add documentation for insert_end function in singly linked list, providing clear, step-by-step instructions on how to add an element to the end of a singly linked list. Additionally, parameter descriptions have been clarified for better understanding

--- linkedlist/CodeVault_LinkedLists/singlyLinkedList/ex02_addingElements.c
+++ linkedlist/CodeVault_LinkedLists/singlyLinkedList/ex02_addingElements.c
@@ -6,8 +6,28 @@
 /*   By: tsoares- <tsoares-@student.42.rio>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/21 13:06:41 by tsoares-          #+#    #+#             */
-/*   Updated: 2024/04/21 13:06:44 by tsoares-         ###   ########.fr       */
+/*   Updated: 2024/04/21 13:30:21 by tsoares-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+/*
+** To add an element to the end of a singly linked list, follow these two steps:
+**
+** 1) Traverse the list to reach the last node;
+** 2) Update the 'next' pointer of the last node to point to the newly created node.
+**
+** Parameters:
+** >> head: A pointer to the pointer to the head of the linked list
+**	    This allows the function to modify the head pointer if necessary;
+** >> data: The data value to be stored in the new node.
+*/
+#include "singly_linked_list.h"
 
+void	insert_end(t_node **head, int data)
+{
+}
+
+int	main(int argc, char **argv)
+{
+	return (0);
+}
