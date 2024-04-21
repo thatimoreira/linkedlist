74ee97ffbea9e86344b0d02a2fbf28a17d150584
thatimoreira <thatimoreira1@gmail.com>
2024-04-21 15:46:49 -0300

    Implement insert_end function to insert nodes at the end of a linked list

--- linkedlist/CodeVault_LinkedLists/singlyLinkedList/ex02_addingElements.c
+++ linkedlist/CodeVault_LinkedLists/singlyLinkedList/ex02_addingElements.c
@@ -6,7 +6,7 @@
 /*   By: tsoares- <tsoares-@student.42.rio>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/21 13:06:41 by tsoares-          #+#    #+#             */
-/*   Updated: 2024/04/21 13:30:21 by tsoares-         ###   ########.fr       */
+/*   Updated: 2024/04/21 15:44:09 by tsoares-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,9 +25,46 @@
 
 void	insert_end(t_node **head, int data)
 {
+	t_node	*new_node;
+	t_node	*current;
+
+	new_node = (t_node *)malloc(sizeof(t_node));
+	if (!new_node)
+		exit(ALLOCATION_FAILURE);
+	new_node->next = NULL;
+	new_node->x = data;
+//	printf("%d\n", new_node->x);
+	current = *head;
+	while (current->next != NULL) // condition to traverse till the last node
+		current = current->next; // 
+	current->next = new_node; // adds new_node to the end of the list
+//	printf("%p\n", current->next);
 }
 
 int	main(int argc, char **argv)
 {
+	int	i;
+	t_node	*head;
+	t_node	*current;
+
+	i = 1;
+	if (argc > 1)
+	{
+		head = (t_node *)malloc(sizeof(t_node));
+		if (!head)
+			return (ALLOCATION_FAILURE);
+		head->x = atoi(argv[i++]);
+		head->next = NULL;
+		while (i < argc)
+			insert_end(&head, atoi(argv[i++]));
+		current = head;
+		while (current != NULL)
+		{
+			printf("%d\n", current->x);
+			current = current->next;
+		}
+	}
+	else
+		printf("Please, inform your list values.\n");
 	return (0);
 }
