f5af517dda48e30bacf11400880b288f09d04d8d
thatimoreira <thatimoreira1@gmail.com>
2024-04-21 12:59:36 -0300

    Basic exercise demonstrating the iteration over a singly linked list

--- linkedlist/CodeVault_LinkedLists/ex01_iteratingOverLinkedList.c
+++ linkedlist/CodeVault_LinkedLists/ex01_iteratingOverLinkedList.c
@@ -6,7 +6,7 @@
 /*   By: tsoares- <tsoares-@student.42.rio>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/21 10:52:53 by tsoares-          #+#    #+#             */
-/*   Updated: 2024/04/21 11:20:51 by tsoares-         ###   ########.fr       */
+/*   Updated: 2024/04/21 12:56:20 by tsoares-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,16 +22,44 @@
 **    and process each node sequentially.
 */
 
-
 #include <stdio.h>
 #include <stdlib.h>
 
+typedef struct s_node
+{
+	int		x;
+	struct s_node	*next;
+}			t_node;
+
+
 int	main(int argc, char **argv)
 {
+	t_node	head;
+	t_node	*current;
+
 	if (argc > 1)
 	{
+		head.x = atoi(argv[1]);
+		head.next = (t_node *)malloc(sizeof(t_node));
+		if (!head.next)
+			return (-1);
+		head.next->x = atoi(argv[2]);
+		head.next->next = (t_node *)malloc(sizeof(t_node));
+		if (!head.next->next)
+			return (-1);
+		head.next->next->x = 22;
+		head.next->next->next = NULL;
+		// Iterating over the list
+		current = &head;
+		while (current != NULL)
+		{
+			printf("%d\n", current->x);
+			current = current->next;
+		}
+		free (head.next->next);
+		free (head.next);
 	}
 	else
-		printf("Please, inform values toi create your linked list.");
+		printf("Please, inform values to create your linked list.\n");
 	return (0);
 }
